b0dd880fdf6c605dc222e4a8f4eed3aadc91f89d
Michael Sotir <sotirm@umich.edu>
2020-04-28 21:05:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,8 +10,14 @@ def introduction(name)
 end
 
 
-introduction_with_language(name, language)
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
+end
 
+def introduction_with_language_optional(naem, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
